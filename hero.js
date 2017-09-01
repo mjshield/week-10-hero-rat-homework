2b863ff95ba1e155799f49d8bb14a022eb283ab8
Matthew Shield <matthewshield@hotmail.co.uk>
2017-09-01 15:11:39 +0100

    Added sorting method, finished MVP

--- hero.js
+++ hero.js
@@ -9,6 +9,36 @@ Hero.prototype = {
 
   talk: function() {
     return "Hello! My name is " + this.name;
+  },
+
+  eat: function(food) {
+    if (food.name === this.favFood){
+      totalReplenishment = food.replenishment * 1.5;
+      this.health += totalReplenishment;
+    }
+    else{ this.health += food.replenishment;}
+  },
+
+  sortTasks: function(sortValue) {
+    switch(sortValue) {
+        case "difficulty":
+            this.tasks.sort(function(a, b) {
+              return a.difficulty - b.difficulty;
+            });
+            break;
+        case "urgency":
+            this.tasks.sort(function(a, b) {
+              return a.urgency - b.urgency;
+            });
+            break;
+        case "reward":
+            this.tasks.sort(function(a, b) {
+              return a.reward - b.reward;
+            });
+            break;
+        default:
+            break;
+    }
   }
 
 }
--- specs/hero_spec.js
+++ specs/hero_spec.js
@@ -1,6 +1,8 @@
 var assert = require("assert");
 var Hero = require("../hero.js");
 var Task = require("../task.js");
+var Food = require("../food.js");
+
 
 
 describe("Hero", function() {
@@ -8,11 +10,16 @@ describe("Hero", function() {
   var hero1;
   var task1;
   var task2;
+  var task3;
 
   beforeEach(function() {
-    task1 = new Task("Finish eating pizza", 1, 3, 5);
-    task2 = new Task("Defeat Snake and his brother Ladders", 3, 3, 180);
+    food1 = new Food("Turkey", 50);
+    food2 = new Food("Burger", 10);
+    task1 = new Task("Finish eating pizza", 1, 2, 80);
+    task2 = new Task("Defeat Snake and his brother Ladders", 3, 1, 35);
+    task3 = new Task("Rescue Keith from the Salmon Gang", 2, 3, 20);
     hero1 = new Hero("Brave Guy", 100, "Turkey", [task1, task2]);
+    hero2 = new Hero("Ragefist", 200, "Burger", [task2, task1, task3]);
   }),
 
   it("has a name", function() {
@@ -33,6 +40,31 @@ describe("Hero", function() {
 
   it("has a task list", function() {
     assert.deepStrictEqual(hero1.tasks, [task1, task2]);
+  }),
+
+  it("can eat food", function() {
+    hero1.eat(food2);
+    assert.strictEqual(hero1.health, 110);
+  }),
+
+  it("can eat favourite food", function() {
+    hero1.eat(food1);
+    assert.strictEqual(hero1.health, 175);
+  }),
+
+  it("can sort tasks by difficulty", function() {
+    hero2.sortTasks("difficulty");
+    assert.deepStrictEqual(hero2.tasks, [task1, task3, task2])
+  }),
+
+  it("can sort tasks by urgency", function() {
+    hero2.sortTasks("urgency");
+    assert.deepStrictEqual(hero2.tasks, [task2, task1, task3])
+  }),
+
+  it("can sort tasks by reward", function() {
+    hero2.sortTasks("reward");
+    assert.deepStrictEqual(hero2.tasks, [task3, task2, task1])
   })
 
 })
